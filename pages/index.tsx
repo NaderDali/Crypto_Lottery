20cfa12d71ebddee8d70b13ff644f0e19b210ff8
unknown <naderdali70@gmail.com>
2022-10-31 01:52:27 +0300

    Computer Based Lottery

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+
+//localhost:3000
+const Home: NextPage = () => {
+  return (
+    <div className="bg-[#091818] min-h-screen flex flex-col">
+      <Head>
+        <title>Computer Based Lottery</title>
+      </Head>
+      <Header />
+      <h1>
+        
+      </h1>
+
+      
+
+      
+    </div>
+  )
+}
+
+export default Home
