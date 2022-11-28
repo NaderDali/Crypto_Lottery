dd3fa9006e0e3d352ade5883b589416108e0bde9
unknown <naderdali70@gmail.com>
2022-11-28 21:42:19 +0300

    adding countdown counter

---
+++ .history/components/CountDownTimer_20221128204930.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {useContract, useContractRead} from "@thirdweb-dev/react"
+import Countdown from 'react-countdown';
+
+type Props={
+    hours:number;
+    minutes:number;
+    seconds:number;
+    completed:boolean;
+};
+function CountDownTimer() {
+    const {contract } =useContract (
+        process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
+     );
+     const { data: expiration, isLoading: isLoadingExpiration } =useContractRead(
+            contract,
+            "expiration"
+     );
+
+     const renderer = ({hours,minutes,seconds,completed}: Props)=> {
+        if(true) { 
+            return (
+              <div>
+                 <h2>Tickets Sales have now closed for this Draw</h2>
+              </div>
+            )
+
+        } else {
+            return (
+            <div>
+                <h3 className="text-white text-sm mb-2 italic"> Time Remaining</h3>
+            </div>
+            )
+        }
+
+     }
+
+             
+  return <div>
+    <Countdown date={ new Date(expiration * 1000)} renderer={renderer} />
+     
+
+  </div>;
+}
+
+export default CountDownTimer
